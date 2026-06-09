9a7a30f94ed8817e4bd53fda2f31692bff336472
Belles <sarah.rose1833@gmail.com>
2026-06-08 21:26:07 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Remove Docker Files
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+# Adds the docker repository to dnf
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+# Installing Docker
+sudo dnf upgrade
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin --assumeyes
+# enable docker service to start on boot
+sudo systemctl enable --now docker
